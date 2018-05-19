0fe787459b16be1c7232bc3d372c210b4dfe39ca
iliyas1155 <iliyas1155@gmail.com>
2018-05-20 02:13:03 +0600

    background

---
+++ app/src/main/java/com/example/iitu/trainapp/Activities/WhatWeDoActivity.java
@@ -0,0 +1,15 @@
+package com.example.iitu.trainapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.iitu.trainapp.R;
+
+public class WhatWeDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_we_do);
+    }
+}
